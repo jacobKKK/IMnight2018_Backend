bca2d62cb54ae5a43f32eb170361709951ba40aa
secret104278 <secret104278@gmail.com>
2017-12-16 22:00:50 +0800

    create models for human

--- IMnight/human/admin.py
+++ IMnight/human/admin.py
@@ -1,3 +1,39 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+from .models import Profile, Relationship
 
-# Register your models here.
+
+class RelationshipInLine(admin.StackedInline):
+    model = Relationship
+    extra = 0
+    exclude = ['created']
+
+
+class ClientInLine(RelationshipInLine):
+    fk_name = 'client'
+
+
+class PerformerInLine(RelationshipInLine):
+    fk_name = 'performer'
+
+
+class ProfileInLine(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    extra = 0
+
+
+class UserAdmin(UserAdmin):
+    inlines = (ProfileInLine, ClientInLine, PerformerInLine,)
+
+    def get_inline_instances(self, request, obj=None):
+        if not obj:
+            return list()
+        return super(UserAdmin, self).get_inline_instances(request, obj)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
+admin.site.register(Relationship)
--- IMnight/human/models.py
+++ IMnight/human/models.py
@@ -1,3 +1,80 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.utils import timezone
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True)
+    birth_date = models.DateField(null=True, blank=True)
+
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
+def is_client(user):
+        # chelk if user is in Clients Group
+    return user.groups.filter(name='Clients').exists()
+
+
+def is_performer(user):
+    # chelk if user is in Performers Group
+    return user.groups.filter(name='Performers').exists()
+
+
+class RelationshipManager(models.Manager):
+    def get_performers(self, user):
+        if is_performer(user):
+            return ValidationError("You can't get performers list from a performer")
+
+        performers = Relationship.objects.filter(client=user)
+        return performers
+
+    def get_clients(self, user):
+        if is_client(user):
+            return ValidationError("You can't get clients list from a client")
+
+        clients = Relationship.objects.filter(client=user)
+        return clients
+
+
+class Relationship(models.Model):
+    client = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='client')
+    performer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='performer')
+    created = models.DateTimeField(default=timezone.now)
+
+    # set the model manager to FriendshipManager()
+    # objects = RelationshipManager()
+
+    class Meta:
+        # verbose_name = 'Relationship'
+        # verbose_name_plural = 'Relationship'
+        unique_together = ('client', 'performer')
+
+    def __unicode__(self):
+        return "Client #%s has relationship with Performer #%s" % (self.client, self.performer)
+
+    def save(self, *args, **kwargs):
+        # Some identity check for the User
+        if not is_client(self.client):
+            raise ValidationError("self.client is not in Clients Group")
+        if not is_performer(self.performer):
+            raise ValidationError("self.performer is not in Performers Group")
+        if self.client == self.performer:
+            raise ValidationError(
+                "self.client and slef.performer can't be same person")
+
+        super(Relationship, self).save(*args, **kwargs)
