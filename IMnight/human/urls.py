ba15ba9ed5a8978e59bec5c6c10b4e53ea22ea20
secret104278 <secret104278@gmail.com>
2017-12-19 00:16:29 +0800

    upgrade

--- IMnight/human/urls.py
+++ IMnight/human/urls.py
@@ -2,8 +2,7 @@ from django.conf.urls import url
 from .views import (SelfDetailsView, UserDetailsView)
 
 urlpatterns = [
-    # URLs that do not require a session or valid token
-    url(r'^user/self/', SelfDetailsView.as_view()),
     url(r'^user/$', UserDetailsView.as_view()),
+    url(r'^user/self/', SelfDetailsView.as_view()),
     url(r'^user/(?P<username>.+)/', UserDetailsView.as_view()),
 ]
