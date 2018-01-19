0aebbed08505d7ad9e65def48d8e3623fcd71eea
secret104278 <secret104278@gmail.com>
2018-01-19 12:50:26 +0800

    upgrade

--- IMnight/human/admin.py
+++ IMnight/human/admin.py
@@ -6,33 +6,35 @@ from human.models import Profile, Relationship
 from human.chat.models import Message
 
 
-class RelationshipInLine(admin.StackedInline):
+class RelationshipInLine(admin.TabularInline):
     model = Relationship
+    # the number of extra forms the formset will display in addition to the initial forms
     extra = 0
-    exclude = ['created']
+    exclude = ('created',)
 
 
 class ClientInLine(RelationshipInLine):
     fk_name = 'client'
+    verbose_name = "Performer"
+    verbose_name_plural = "My Performer"
 
 
 class PerformerInLine(RelationshipInLine):
     fk_name = 'performer'
+    verbose_name = "Client"
+    verbose_name_plural = "My Client"
 
 
 class ProfileInLine(admin.StackedInline):
     model = Profile
+    # there must exactly have one Profile link to one User
     can_delete = False
+    # the number of extra forms the formset will display in addition to the initial forms
     extra = 0
 
 
 class UserAdmin(admin.ModelAdmin):
     inlines = (ProfileInLine, ClientInLine, PerformerInLine,)
-
-    def get_inline_instances(self, request, obj=None):
-        if not obj:
-            return list()
-        return super(UserAdmin, self).get_inline_instances(request, obj)
 
 
 class RelationshipAdmin(admin.ModelAdmin):
