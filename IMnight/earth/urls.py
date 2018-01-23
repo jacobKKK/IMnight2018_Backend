257ecd482d0b24e95fe56bbc5976042249564f85
secret104278 <secret104278@gmail.com>
2018-01-24 00:38:34 +0800

    upgrade

--- IMnight/earth/urls.py
+++ IMnight/earth/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     url(r'^vocher/$', StoreVocherView.as_view()),
     url(r'^vocher/(?P<storename>.+)/$', StoreVocherView.as_view()),
     url(r'^daily/$', DailyVocherView.as_view()),
-    url(r'^vocher/user/(?P<label>.+)/$', UseVocherView.as_view()),
+    url(r'^vocher/use/(?P<label>.+)/$', UseVocherView.as_view()),
 ]
