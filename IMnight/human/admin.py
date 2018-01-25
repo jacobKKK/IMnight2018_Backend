a51b172f538b933c5fcf3ce83b013793bdcaefd7
secret104278 <secret104278@gmail.com>
2018-01-25 20:24:01 +0800

    registe Task, Reward to admin

--- IMnight/human/admin.py
+++ IMnight/human/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.contrib.auth.admin import UserAdmin
 
-from human.models import Profile, Relationship
+from human.models import Profile, Relationship, Reward, Task
 from human.chat.models import Message
 
 
@@ -50,3 +50,5 @@ admin.site.register(User, UserAdmin)
 admin.site.register(Profile)
 admin.site.register(Relationship, RelationshipAdmin)
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Reward)
+admin.site.register(Task)
