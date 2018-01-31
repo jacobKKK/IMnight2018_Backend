058b84c68aa2b797bdd7997a9909116b2918629e
secret104278 <secret104278@gmail.com>
2018-01-31 12:50:40 +0800

    heroku

--- IMnight/IMnight/production_settings.py
+++ IMnight/IMnight/production_settings.py
@@ -20,3 +20,74 @@ ALLOWED_HOSTS = ['*']
 
 # Turn off DEBUG mode.
 DEBUG = False
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s - [%(asctime)s] (%(module)s %(process)d %(thread)d) %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+        'simple': {
+            'format': '%(levelname)s - [%(asctime)s] %(message)s',
+            'datefmt': '%Y-%m-%d %H:%M:%S'
+        },
+    },
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        },
+        'development_logfile': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_true'],
+            'class': 'logging.FileHandler',
+            'filename': 'IMnight/tmp/django_dev.log',
+            'formatter': 'verbose'
+        },
+        'production_logfile': {
+            'level': 'ERROR',
+            'filters': ['require_debug_false'],
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': 'IMnight/tmp/django_production.log',
+            'maxBytes': 1024 * 1024 * 100,  # 100MB
+            'backupCount': 5,
+            'formatter': 'simple'
+        },
+        'dba_logfile': {
+            'level': 'DEBUG',
+            'filters': ['require_debug_false', 'require_debug_true'],
+            'class': 'logging.handlers.WatchedFileHandler',
+            'filename': 'IMnight/tmp/django_dba.log',
+            'formatter': 'simple'
+        },
+    },
+    'loggers': {
+        'testdevelop': {
+            'handlers': ['console', ],
+        },
+        'coffeehouse': {
+            'handlers': ['development_logfile', 'production_logfile'],
+        },
+        'dba': {
+            'handlers': ['dba_logfile'],
+        },
+        'django': {
+            'handlers': ['development_logfile', 'production_logfile'],
+        },
+        'py.warnings': {
+            'handlers': ['development_logfile'],
+        },
+    },
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse',
+        },
+        'require_debug_true': {
+            '()': 'django.utils.log.RequireDebugTrue',
+        },
+    },
+}
