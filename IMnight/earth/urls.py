d6ad0d190c467e87db903f1200380ec1d0ef742b
secret104278 <secret104278@gmail.com>
2018-02-05 12:20:00 +0800

    minor bug fix

--- IMnight/earth/urls.py
+++ IMnight/earth/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-from earth.views import DailyVocherView, StoreVocherView, hello_world
+from earth.views import DailyVocherView, StoreVocherView, use_vocher
 
 urlpatterns = [
     url(r'^vocher/$', StoreVocherView.as_view()),
     url(r'^vocher/(?P<storename>.+)/$', StoreVocherView.as_view()),
     url(r'^daily/$', DailyVocherView.as_view()),
-    url(r'^use/vocher/$', hello_world),
+    url(r'^use/vocher/$', use_vocher),
 ]
