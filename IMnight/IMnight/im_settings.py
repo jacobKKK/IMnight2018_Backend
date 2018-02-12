0e3dcc3e858f26dc030c163d0b3435ee51b32bde
YuChih <secret104278@gmail.com>
2018-02-12 20:20:34 +0800

    upgrade

--- IMnight/IMnight/im_settings.py
+++ IMnight/IMnight/im_settings.py
@@ -5,7 +5,7 @@ STATIC_URL = '/static/IMnight/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Allow all host headers.
-ALLOWED_HOSTS = ['140.112.106.45']
+ALLOWED_HOSTS = ['140.112.106.45','ntu.im']
 
 # Turn off DEBUG mode.
 #DEBUG = False
