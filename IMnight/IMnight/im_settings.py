81133598ecf2ae283451dfb419dacb21a6ff2daa
secret104278 <secret104278@gmail.com>
2018-02-21 17:40:49 +0800

    all CORS

--- IMnight/IMnight/im_settings.py
+++ IMnight/IMnight/im_settings.py
@@ -1,11 +1,13 @@
-#import all default settings.
+# import all default settings.
 from .settings import *
 
 STATIC_URL = '/static/IMnight/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 # Allow all host headers.
-ALLOWED_HOSTS = ['140.112.106.45','ntu.im']
+ALLOWED_HOSTS = ['140.112.106.45', 'ntu.im']
 
 # Turn off DEBUG mode.
 #DEBUG = False
